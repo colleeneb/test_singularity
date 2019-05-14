551a39f48aae4c307ebfd78667441e9902a29b0f
Colleen Bertoni <bertoni@anl.gov>
2019-05-14 14:58:45 -0500

    test

---
+++ example_codes/pi.c
@@ -0,0 +1,15 @@
+#include <math.h>
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+  int n, myid, numprocs, i;
+  double PI25DT = 3.141592653589793238462643;
+  double mypi, pi, h, sum, x;
+
+
+  /* Number of intervals */
+  n = 25;
+  printf( "Hello World\n" );
+  return 0;
+}
